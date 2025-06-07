547fd9fb1e05e3b1a2bf3763fdd4c9497234b5a6
andriffx <dgmaniac@hotmail.com>
2025-06-07 12:10:45 +0800

    Sesi1: Setup&Fetch Data API

---
+++ ZenAnimeList Project/zenanimelist/src/app/search/page.js
@@ -0,0 +1,14 @@
+const Page = () => {
+  return (
+    <>
+      <h1> SEARCH PAGE ARROW FUNC </h1>
+      <p>Ini adalah search page</p>
+    </>
+  );
+};
+
+export default Page;
+
+// export default function Page(){
+//     return <div>SEARCH PAGE</div>
+// }
---
+++ ZenAnimeList Project/zenanimelist/src/components/Search/index.js
@@ -0,0 +1,10 @@
+const Search = () => {
+  return(
+    <div>
+    <h1>ANIME SEARCH COMPONENT</h1>
+    <p>Ini adalah halaman Search Component</p>
+    </div>
+  )
+}
+
+export default Search
